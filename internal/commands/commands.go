22391cf33947ae048fa78d9fdc0b293f949653b8
charlesaraya <charlesaraya@gmail.com>
2025-05-24 11:25:59 +0200

    refactor: improve troubleshooting + make error handling more idiomatic

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -27,13 +27,20 @@ type Commands struct {
 }
 
 func (c *Commands) Run(s *State, cmd Command) error {
-	if err := c.CommandRegistry[cmd.Name](s, cmd); err != nil {
-		return err
+	cmdHandler, ok := c.CommandRegistry[cmd.Name]
+	if !ok {
+		return fmt.Errorf("command '%s' not registered", cmd.Name)
+	}
+	if err := cmdHandler(s, cmd); err != nil {
+		return fmt.Errorf("failed to run command '%s': %w", cmd.Name, err)
 	}
 	return nil
 }
 
 func (c *Commands) Register(name string, f func(*State, Command) error) error {
+	if _, ok := c.CommandRegistry[name]; ok {
+		return fmt.Errorf("command '%s' already registered", name)
+	}
 	c.CommandRegistry[name] = f
 	return nil
 }
@@ -46,17 +53,14 @@ func GetCommands() Commands {
 
 func LoginHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called with no arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s <userName>", cmd.Name)
 	}
 	userName := cmd.Arguments[0]
-
-	_, err := s.Db.GetUser(context.Background(), userName)
-	if err != nil {
-		return err
+	if _, err := s.Db.GetUser(context.Background(), userName); err != nil {
+		return fmt.Errorf("failed to get user: %w", err)
 	}
-
 	if err := s.Config.SetUser(userName); err != nil {
-		return err
+		return fmt.Errorf("failed to set user in config: %w", err)
 	}
 	log.Printf("Login: %s", userName)
 	return nil
@@ -64,7 +68,7 @@ func LoginHandler(s *State, cmd Command) error {
 
 func RegisterHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called with no arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s <userName>", cmd.Name)
 	}
 	userName := cmd.Arguments[0]
 	userParams := database.CreateUserParams{
@@ -75,10 +79,10 @@ func RegisterHandler(s *State, cmd Command) error {
 	}
 	user, err := s.Db.CreateUser(context.Background(), userParams)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create user: %w", err)
 	}
 	if err = s.Config.SetUser(userName); err != nil {
-		return err
+		return fmt.Errorf("failed to set user in config: %w", err)
 	}
 	log.Printf("Register: %s(%s)", user.Name, user.ID.String())
 	return nil
@@ -86,12 +90,13 @@ func RegisterHandler(s *State, cmd Command) error {
 
 func UsersHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 0 {
-		return fmt.Errorf("%s called with arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s", cmd.Name)
 	}
 	users, err := s.Db.GetUsers(context.Background())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get users: %w", err)
 	}
+	log.Printf("Users: %v users", len(users))
 	for _, user := range users {
 		if user.Name == s.Config.UserName {
 			fmt.Printf("* %s (current)\n", user.Name)
@@ -104,11 +109,11 @@ func UsersHandler(s *State, cmd Command) error {
 
 func ResetHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 0 {
-		return fmt.Errorf("%s called with arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage. use: %s", cmd.Name)
 	}
 	err := s.Db.DeleteUsers(context.Background())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to delete users: %w", err)
 	}
 	log.Printf("Reset")
 	return nil
@@ -116,17 +121,17 @@ func ResetHandler(s *State, cmd Command) error {
 
 func AggregateFeedHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called no arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage. use: %s <timeBetweenRequests>", cmd.Name)
 	}
 	timeBetweenRequests, err := time.ParseDuration(cmd.Arguments[0])
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to parse duration from argument: %w", err)
 	}
 	log.Printf("Aggregate Feed: collecting feeds every %v\n", timeBetweenRequests)
 	ticker := time.NewTicker(timeBetweenRequests)
 	for ; ; <-ticker.C {
 		if err = scrapeFeeds(s); err != nil {
-			return err
+			return fmt.Errorf("failed to scrape feeds: %w", err)
 		}
 	}
 }
@@ -134,14 +139,14 @@ func AggregateFeedHandler(s *State, cmd Command) error {
 func scrapeFeeds(s *State) error {
 	feed, err := s.Db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get next feed to fetch: %w", err)
 	}
 	fetchedFeed, err := rss.FetchFeed(context.Background(), feed.Url)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to fetch feed: %w", err)
 	}
 	if err = s.Db.MarkFeedFetched(context.Background(), feed.ID); err != nil {
-		return err
+		return fmt.Errorf("failed to mark feed as fetched: %w", err)
 	}
 	log.Printf("Fetched: %s (%v items)\n", feed.Name, len(fetchedFeed.Channel.Items))
 	for i, item := range fetchedFeed.Channel.Items {
@@ -151,11 +156,10 @@ func scrapeFeeds(s *State) error {
 }
 
 func AddFeedHandler(s *State, cmd Command, user database.User) error {
-	if len(cmd.Arguments) < 2 {
-		return fmt.Errorf("%s called with not enough arguments", cmd.Name)
+	if len(cmd.Arguments) != 2 {
+		return fmt.Errorf("incorrect command usage.\nusage: %s <feedName> <feedUrl>", cmd.Name)
 	}
-	feedName := cmd.Arguments[0]
-	feedUrl := cmd.Arguments[1]
+	feedName, feedUrl := cmd.Arguments[0], cmd.Arguments[1]
 	feedParams := database.CreateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now(),
@@ -166,7 +170,7 @@ func AddFeedHandler(s *State, cmd Command, user database.User) error {
 	}
 	feed, err := s.Db.CreateFeed(context.Background(), feedParams)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create feed: %w", err)
 	}
 	feedFollowParams := database.CreateFeedFollowParams{
 		ID:        uuid.New(),
@@ -177,7 +181,7 @@ func AddFeedHandler(s *State, cmd Command, user database.User) error {
 	}
 	_, err = s.Db.CreateFeedFollow(context.Background(), feedFollowParams)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to follow feed: %w", err)
 	}
 	fmt.Println(feed)
 	log.Printf("Add Feed: '%s' added '%s' (%s)", user.Name, feedName, feedUrl)
@@ -186,12 +190,12 @@ func AddFeedHandler(s *State, cmd Command, user database.User) error {
 
 func DeleteFeedHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called with no arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s <feedUrl>", cmd.Name)
 	}
 	feedUrl := cmd.Arguments[0]
 	err := s.Db.DeleteFeed(context.Background(), feedUrl)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to delete feed: %w", err)
 	}
 	log.Printf("Delete Feed: %s", feedUrl)
 	return nil
@@ -199,12 +203,12 @@ func DeleteFeedHandler(s *State, cmd Command) error {
 
 func FeedsHandler(s *State, cmd Command) error {
 	if len(cmd.Arguments) != 0 {
-		return fmt.Errorf("%s called with arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s", cmd.Name)
 	}
 
 	feeds, err := s.Db.GetUserFeeds(context.Background())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get all feeds: %w", err)
 	}
 	for _, feed := range feeds {
 		fmt.Printf("* %s(%s) from %s\n", feed.Name, feed.Url, feed.UserName)
@@ -215,12 +219,12 @@ func FeedsHandler(s *State, cmd Command) error {
 
 func FollowFeedsHandler(s *State, cmd Command, user database.User) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called with no arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s <feedUrl>", cmd.Name)
 	}
 	feedUrl := cmd.Arguments[0]
 	feed, err := s.Db.GetFeed(context.Background(), feedUrl)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get feed: %w", err)
 	}
 	feedFollowParams := database.CreateFeedFollowParams{
 		ID:        uuid.New(),
@@ -231,7 +235,7 @@ func FollowFeedsHandler(s *State, cmd Command, user database.User) error {
 	}
 	feed_follow, err := s.Db.CreateFeedFollow(context.Background(), feedFollowParams)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to follow feed: %w", err)
 	}
 	log.Printf("Follow: '%s' followed '%s' feed\n", feed_follow.UserName, feed_follow.FeedName)
 	return nil
@@ -239,11 +243,11 @@ func FollowFeedsHandler(s *State, cmd Command, user database.User) error {
 
 func FollowedFeedsHandler(s *State, cmd Command, user database.User) error {
 	if len(cmd.Arguments) != 0 {
-		return fmt.Errorf("%s called with arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s", cmd.Name)
 	}
 	feeds, err := s.Db.GetFeedFollowsForUser(context.Background(), user.ID)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get followed feeds: %w", err)
 	}
 	for _, feed := range feeds {
 		fmt.Printf("* %s follows %s\n", user.Name, feed.FeedName)
@@ -254,7 +258,7 @@ func FollowedFeedsHandler(s *State, cmd Command, user database.User) error {
 
 func UnFollowFeedHandler(s *State, cmd Command, user database.User) error {
 	if len(cmd.Arguments) != 1 {
-		return fmt.Errorf("%s called with not enough arguments", cmd.Name)
+		return fmt.Errorf("incorrect command usage.\nusage: %s <feedUrl>", cmd.Name)
 	}
 	feedUrl := cmd.Arguments[0]
 	params := database.DeleteFeedFollowParams{
@@ -263,7 +267,7 @@ func UnFollowFeedHandler(s *State, cmd Command, user database.User) error {
 	}
 	_, err := s.Db.DeleteFeedFollow(context.Background(), params)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to delete feed: %w", err)
 	}
 	log.Printf("Unfollow '%s' unfollowed '%s'\n", user.Name, feedUrl)
 	return nil
