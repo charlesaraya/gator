6541266fe59e85e11472825b5cb71abda0527da6
charlesaraya <charlesaraya@gmail.com>
2025-05-24 11:35:14 +0200

    refactor: improve troubleshooting + make error handling more idiomatic

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -13,10 +14,10 @@ const (
 	CONFIG_FILE string = ".gatorconfig.json"
 )
 
-func getConfigFile() (string, error) {
+func getConfigFilePath() (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to get home dir: %w", err)
 	}
 	return filepath.Join(home, CONFIG_FILE), nil
 }
@@ -30,31 +31,31 @@ func (c *Config) SetUser(name string) error {
 	c.UserName = name
 	data, err := json.Marshal(c)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal confiration: %w", err)
 	}
-	configFile, err := getConfigFile()
+	configFile, err := getConfigFilePath()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get config file path: %w", err)
 	}
 	if err = os.WriteFile(configFile, data, 0664); err != nil {
-		return err
+		return fmt.Errorf("failed to write configuration file: %w", err)
 	}
 	return nil
 }
 
 func Read() (Config, error) {
 	config := Config{}
-	configFile, err := getConfigFile()
+	configFile, err := getConfigFilePath()
 	if err != nil {
-		return config, err
+		return config, fmt.Errorf("failed to get configuration file: %w", err)
 	}
 	data, err := os.ReadFile(configFile)
 	if err != nil {
-		return config, err
+		return config, fmt.Errorf("failed to read configuration file: %w", err)
 	}
 	err = json.Unmarshal(data, &config)
 	if err != nil {
-		return config, err
+		return config, fmt.Errorf("failed to unmarshal configuration: %w", err)
 	}
 	return config, nil
 }
@@ -62,7 +63,7 @@ func Read() (Config, error) {
 func LoadDB(cfg *Config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.DBUrl)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open db: %w", err)
 	}
 	return db, nil
 }
--- internal/rss/rss.go
+++ internal/rss/rss.go
@@ -3,6 +3,7 @@ package rss
 import (
 	"context"
 	"encoding/xml"
+	"fmt"
 	"html"
 	"io"
 	"net/http"
@@ -27,24 +28,24 @@ type RSSItem struct {
 func FetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, feedUrl, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create new request: %w", err)
 	}
 	req.Header.Set("User-Agent", "gator")
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to send request: %w", err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response body: %w", err)
 	}
 	feed := RSSFeed{}
 	err = xml.Unmarshal(body, &feed)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unmarshal body as a RRS feed: %w", err)
 	}
 	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
 	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
